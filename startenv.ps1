2036e0b73649acabef72a4c1998842cd6de519b2
Gbrvi <gv.almeida06@gmail.com>
2025-05-18 18:32:23 -0300

    Generic command to everyone

--- startenv.ps1
+++ startenv.ps1
@@ -1,2 +1,11 @@
+# Caminho relativo para o script de ativação
+$activateScript = Join-Path -Path $PSScriptRoot -ChildPath "myenv\Scripts\Activate.ps1"
 
-. 'C:\Users\gvalm\Documents\Projetos\WordCounter\myenv\Scripts\Activate.ps1'
+# Verifica se o arquivo de ativação existe
+if (Test-Path $activateScript) {
+    # Executa o script de ativação
+    & $activateScript
+} else {
+    Write-Host "Arquivo de ativação não encontrado em: $activateScript"
+    Write-Host "Certifique-se de que o ambiente virtual está criado na pasta correta."
+}
